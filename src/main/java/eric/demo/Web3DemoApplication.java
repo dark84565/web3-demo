7ee512861d3c0438d34f7837a543b62ec206aca7
Eric Chang <>
2024-09-24 02:08:57 +0800

    project init

---
+++ src/main/java/eric/demo/Web3DemoApplication.java
@@ -0,0 +1,13 @@
+package eric.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web3DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web3DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eric/demo/Web3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eric.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web3DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
